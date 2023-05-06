ea392b2f55da439459c9189465e6d07376894730
Daniel <daniel0ar@hotmail.com>
2023-05-05 22:36:28 -0500

    Add imports to use hardhat and chainlink nodes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@chainlink/hardhat-chainlink";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
